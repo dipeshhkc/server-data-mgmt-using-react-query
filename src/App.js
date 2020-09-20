bbad05cf449bc1e20fc1aea603e0720164c53b17
dipesh <kcdipesh429@gmail.com>
2020-09-20 21:06:20 +0545

    feat:add skeleton react query

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useQuery } from 'react-query'
 
 function App() {
+  const { isLoading, error, data } = useQuery('repoData', () =>
+    fetch('https://jsonplaceholder.typicode.com/posts/1').then((res) =>
+      res.json()
+    )
+  );
+  if (isLoading) return 'Loading...';
+
+  if (error) return 'An error has occurred: ' + error.message;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>{data.title}</h1>
+      <p>{data.body}</p>
+      
     </div>
   );
 }
